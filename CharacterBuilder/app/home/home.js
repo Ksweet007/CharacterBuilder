d0807bbacf60777842d8979d1df88375078ddd4c
KyleSweetMeM <kyle.sweet@mem.com>
2017-02-27 21:39:10 -0500

    Rebuilding File Structure

---
+++ CharacterBuilder/app/home/home.js
@@ -0,0 +1,19 @@
+﻿define(function (require) {
+    var _i = {
+        ko: require('knockout'),
+        $: require('jquery'),
+        alert: require('_custom/services/alert')
+    };
+
+    return function () {
+        var self = this;
+        self.data = null;
+
+        self.activate = function (classId) {
+
+        }//END RETURN
+
+
+    };//END ACTIVATE
+
+});
