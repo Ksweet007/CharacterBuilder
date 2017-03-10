a28aac72bd501e7bfaf6789861b3b2bb4a44a954
KyleSweetMeM <kyle.sweet@mem.com>
2017-03-09 19:17:06 -0500

    Ability Scores can now be rolled

--- CharacterBuilder/app/home/home.js
+++ CharacterBuilder/app/home/home.js
@@ -51,24 +51,8 @@
             _i.charajax.get('api/charactersheet/GetUserSheets').done(function (response) {
                 response.forEach(function (sheet) {
                     sheet.createdDateFormatted = moment(sheet.CreatedDate).format('LLL');
-                 
-                    sheet.AbilityScoreIncreases.forEach(function (item) {
-                        switch (item.Name) {
-                            case "Strength":
-                                sheet.Strength += item.IncreaseAmount;
-                            case "Dexterity":
-                                sheet.Dexterity += item.IncreaseAmount;
-                            case "Constitution":
-                                sheet.Constitution += item.IncreaseAmount;
-                            case "Intelligence":
-                                sheet.Intelligence += item.IncreaseAmount;
-                            case "Wisdom":
-                                sheet.Wisdom += item.IncreaseAmount;
-                            case "Charisma":
-                                sheet.Charisma += item.IncreaseAmount;
-                        }
-                    });
-                    
+                    self.addAbilityScoreIncreasesToScores(sheet);
+                    self.calculateAbilityModifiers(sheet);
                 });
                 var mapped = _i.ko.mapping.fromJS(response);
 
@@ -76,6 +60,89 @@
                 deferred.resolve();
             });
             return deferred;
+        };
+
+        self.addAbilityScoreIncreasesToScores = function (sheet) {
+            sheet.strBonus = 0;
+            sheet.dexBonus = 0;
+            sheet.conBonus = 0;
+            sheet.intBonus = 0;
+            sheet.wisBonus = 0;
+            sheet.chaBonus = 0;
+            sheet.AbilityScoreIncreases.forEach(function (item) {
+                switch (item.Name) {
+                    case "Strength":
+                        sheet.Strength += item.IncreaseAmount;
+                        sheet.strBonus = item.IncreaseAmount;
+                        break;
+                    case "Dexterity":
+                        sheet.Dexterity += item.IncreaseAmount;
+                        sheet.dexBonus = item.IncreaseAmount;
+                        break;
+                    case "Constitution":
+                        sheet.Constitution += item.IncreaseAmount;
+                        sheet.conBonus = item.IncreaseAmount;
+                        break;
+                    case "Intelligence":
+                        sheet.Intelligence += item.IncreaseAmount;
+                        sheet.intBonus = item.IncreaseAmount;
+                        break;
+                    case "Wisdom":
+                        sheet.Wisdom += item.IncreaseAmount;
+                        sheet.wisBonus = item.IncreaseAmount;
+                        break;
+                    case "Charisma":
+                        sheet.Charisma += item.IncreaseAmount;
+                        sheet.chaBonus = item.IncreaseAmount;
+                        break;
+                }
+            });
+        };
+
+        self.calculateAbilityModifiers = function(sheet) {
+            sheet.StrengthMod = Math.floor((sheet.Strength - 10) / 2);
+            sheet.DexterityMod = Math.floor((sheet.Dexterity - 10) / 2);
+            sheet.ConstitutionMod = Math.floor((sheet.Constitution - 10) / 2);
+            sheet.IntelligenceMod = Math.floor((sheet.Intelligence - 10) / 2);
+            sheet.WisdomMod = Math.floor((sheet.Wisdom - 10) / 2);
+            sheet.CharismaMod = Math.floor((sheet.Charisma - 10) / 2);
+        };
+
+        self.calculateSingleModifier = function(abil) {
+            return Math.floor((abil - 10) / 2);
+        };
+
+        self.setScoreOnRoll = function(abil,bonus,mod) {
+            var rolledValue = self.rollAbilityScore();
+            var newScore = rolledValue + bonus;
+            var newMod = Math.floor((newScore - 10) / 2);
+
+            abil(newScore);
+            mod(newMod);
+        };
+
+        self.rollStr = function () {
+            self.setScoreOnRoll(self.selectedSheet().Strength, self.selectedSheet().strBonus(), self.selectedSheet().StrengthMod);           
+        };
+
+        self.rollDex = function () {
+            self.setScoreOnRoll(self.selectedSheet().Dexterity,self.selectedSheet().dexBonus(), self.selectedSheet().DexterityMod);
+        };
+
+        self.rollCon = function () {
+            self.setScoreOnRoll(self.selectedSheet().Constitution,self.selectedSheet().conBonus(), self.selectedSheet().ConstitutionMod);
+        };
+
+        self.rollInt = function () {
+            self.setScoreOnRoll(self.selectedSheet().Intelligence,self.selectedSheet().intBonus(), self.selectedSheet().IntelligenceMod);
+        };
+
+        self.rollWis = function () {
+            self.setScoreOnRoll(self.selectedSheet().Wisdom, self.selectedSheet().wisBonus(),self.selectedSheet().WisdomMod);
+        };
+
+        self.rollCha = function () {
+            self.setScoreOnRoll(self.selectedSheet().Charisma, self.selectedSheet().chaBonus(),self.selectedSheet().CharismaMod);
         };
 
         self.rollAbilityScore = function () {
@@ -93,7 +160,7 @@
             for (var s in rolls) {
                 scoreTotal += rolls[s];
             }
-
+            return scoreTotal;
         };
 
         self.viewMoreDetails = function (bgSelected) {
